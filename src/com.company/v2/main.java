ae365229bd706b213fa47fd5f5ba609dd2675d28
ISSIWARA KAVINDA <65284203+Issiwara@users.noreply.github.com>
2021-08-22 21:09:23 +0530

    V2 Final Code

--- src/com.company/v2/main.java
+++ src/com.company/v2/main.java
@@ -1,25 +1,53 @@
 package com.company.v2;
 
-public class main {
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.List;
+import java.nio.file.Files;
 
-    public static void main(String[] args) {
-        
-        if(args.length == 0)
-        {
+public class Main
+ {
+
+
+    //SOLID PRINCIPLES --> ALL THE SOLID PRINCIPLES ARE VIOLATED HERE!!!
+    public static void main(String[] args) throws IOException {
+
+        //Make sure to validate the arguments before using...
+        if (args.length == 0) {
             System.out.println("Please provide the operation as an argument");
             return;
         }
-    
-    //another validation that we could do to this args
-    //add,sub,mul
 
-    if(!(operator.equals("add")||operator.equals("sub")||operator.equals("mul")))
-    {
-        System.out.println("Please provide add,sub or mul as the operator argument.");
-        return;
+        String operator = args[0];
+
+        if (!(operator.equals("add") || operator.equals("sub") || operator.equals("mul"))) {
+            System.out.println("Please provide add,sub or mul as the operator argument");
+            return;
+        }
+
+        //read the numbers text file
+        List<String> numbersStrs = Files.readAllLines(
+                Paths.get("E:/Desktop/2nd Year/Software Construction/calculator/numbers.txt")
+        );
+
+        double number1 = Double.parseDouble(numbersStrs.get(0));
+        double number2 = Double.parseDouble(numbersStrs.get(1));
+
+        double result = 0;
+
+        switch (operator) {
+            case "add":
+                result = number1 + number2;
+                break;
+            case "sub":
+                result = number1 - number2;
+                break;
+            case "mul":
+                result = number1 * number2;
+                break;
+        }
+
+        System.out.println("The result is " + result);
+
     }
-
-    System.out.println(args[0]);
-}
-
 }
