0dbe8eaf2bf4a74682d5c516b2cb43842ac5d2d7
ISSIWARA KAVINDA <65284203+Issiwara@users.noreply.github.com>
2021-08-22 20:09:16 +0530

    Add files via upload

---
+++ src/java/com.kelaniya.uni/basicCal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class basicCal {
+    public static void main(String[] args) throws IOException {
+
+        Scanner uInput = new Scanner(System.in);
+        System.out.println("Enter an option : Addition = 1 or Subscript = 2 or Multiplication =3");
+        int operator = uInput.nextInt();
+
+        Scanner s = new Scanner(new File("E:\\Desktop\\oop lankadroid\\Interfacee\\sample.TXT"));
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();
+
+        if (operator==1) {
+
+            System.out.println(n1+n2);
+        }
+        else if (operator==2) {
+            System.out.println(n1-n2);
+        }
+        else if (operator==3) {
+
+            System.out.println(n1*n2);
+        }
+
+    }
+}
