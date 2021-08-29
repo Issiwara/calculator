6dbcb39af5ac10db06c746a3cdd974b073802c8c
ISSIWARA KAVINDA <65284203+Issiwara@users.noreply.github.com>
2021-08-29 20:28:51 +0530

    Add files via upload

---
+++ src/com.company/v3/Operations/AddOperation.java
@@ -0,0 +1,9 @@
+package com.company.v3;
+
+public class AddOperation {
+
+    public Double eeecute(Double[] numbers){
+        return numbers[0]+numbers[1];
+    }
+    
+}
---
+++ src/com.company/v3/Operations/SubOperation.java
@@ -0,0 +1,9 @@
+package com.company.v3;
+
+public class SubOperation {
+
+    public Double eeecute(Double[] numbers){
+        return numbers[0]-numbers[1];
+    }
+    
+}
